f3729a731651ae3d0e380eda3eae707cf6c68a6a
ajoy-code-dreamer <ajoy.code.dreamer@gmail.com>
2023-12-06 19:13:41 +0600

    added live counter section

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+
+// ===== jQuery operation =====
+$(function(){
+    // === counter up ===
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+    // === counter up ===
+
+
+
+})
+// ===== jQuery operation =====
