ad24d3128d1166a56803d1ad465fe52bdfd5cbae
ajoy-code-dreamer <ajoy.code.dreamer@gmail.com>
2023-12-09 22:49:45 +0600

    complete portfolio_details page

--- js/script.js
+++ js/script.js
@@ -6,5 +6,14 @@ $(function () {
         time: 1000,
     });
     // === counter up ===
+    // === slick slider operation ===
+    $(".portfolio_slider").slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: ".prev_arrow",
+        nextArrow: ".next_arrow",
+    });
+    // === slick slider operation ===
 });
 // ===== jQuery operation =====
